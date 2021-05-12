bf07ffd10f408a3142f6a9e3ab3ad8aca5cae811
JustJeo <j.cabrigas.writer@gmail.com>
2021-05-12 16:24:27 -0700

    starts second test

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-document.getElementById("btn").addEventListener("click", sum)
+document.getElementById("btn").addEventListener("click", timer)
 
-  function sum () {
+  function timer () {
     let num1 = parseInt(prompt("Enter your first number: "))
     let num2 = parseInt(prompt("Enter your second number: "))
     let results = (num1 + num2)
-    document.getElementById("demoText").innerHTML = results
-}
+    console.log(results)
+    const timeInterval = setInterval(() => {
+      if (results === 0) {
+        clearInterval(timeInterval)
+      } else {
+        results--
+        console.log(results)
+      }
+    }, 1000)  
+  document.getElementById("demoText").innerHTML = results
+  }
+
+document.getElementById("keyboard").addEventListener("click", board)
+
+  function board() {
+    
+  }
